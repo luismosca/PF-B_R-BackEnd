f6b833a23a10543e4864c76009538e2971ad015d
Piero <piero7210@hotmail.com>
2023-09-15 12:10:48 -0500

    Model Report completo y postReport handler, controller

--- src/routes/reportsRouter.js
+++ src/routes/reportsRouter.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
-const { getReportsHandler, getReportByIdHandler } = require("../handlers/reportsHandler");
+const { getReportsHandler, getReportByIdHandler, postReportHandler } = require("../handlers/reportsHandler");
 
 const reportsRouter = Router();
 
 reportsRouter.get("/", getReportsHandler); // general y name
 reportsRouter.get("/:id", getReportByIdHandler); // para searchBar
-reportsRouter.post("/"); // para publicar
+reportsRouter.post("/", postReportHandler); // para publicar
 
 
 module.exports = reportsRouter;
