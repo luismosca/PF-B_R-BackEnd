5f2c09be187aa7558539c6b814593d73ad6aa01f
luismosca <luismosca@gmail.com>
2023-09-21 19:49:45 -0300

    deploy back

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const server = require('./src/app.js');
 const { conn, Report } = require('./src/db.js');
-const reports = require("./src/utils/Reports.js")
+const reports = require('./src/utils/Reports.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync().then(() => {
   server.listen(3001, () => {
     console.log('listening at 3001'); // eslint-disable-line no-console
   });
-})
-.then(async ()=>{ 
-  const reportDb = await Report.bulkCreate(reports, { ignoreDuplicates: true })
-  // console.log(reportDb);
-  console.log(`%s Test reports were added to the DB`);
-}) // Para cargar los reportes de prueba al iniciar el servidor
+});
+// .then(async () => {
+//   const reportDb = await Report.bulkCreate(reports, {
+//     ignoreDuplicates: true,
+//   });
+//   // console.log(reportDb);
+//   console.log(`%s Test reports were added to the DB`);
+// }); // Para cargar los reportes de prueba al iniciar el servidor
