f174d07a186b71e15515a5d1843461eda0c9e881
Wilder Ap <wilderasprilla321@gmail.com>
2023-10-01 23:38:08 -0500

    Cambiecitos

--- src/db.js
+++ src/db.js
@@ -2,15 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DATABASE_URL } = process.env;
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,
+} = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false, // Desactiva el registro de consultas SQL si lo deseas
-  dialect: 'postgres', // Indica que estás utilizando PostgreSQL
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: { require: true, rejectUnauthorized: false }, // Habilita SSL para conexiones seguras (importante en Render)
-  },
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/byr`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 // const sequelize = new Sequelize(DATABASE_URL, {   
 //     logging: false, // set to console.log to see the raw SQL queries
